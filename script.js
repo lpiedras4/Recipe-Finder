35e2950c20400cb308c142141dbc126d4be7c4cb
lpiedras4 <piedras.leo@gmail.com>
2026-01-15 19:00:38 -0800

    started script to fetch data

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hola perros");
+async function getData(name){
+    const url = "www.themealdb.com/api/json/v1/1/search.php?s={name}";
+    try{
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const result = await response.json();
+        console.log(result);
+    }catch (error){
+        console.log(error.message);
+    }
+}
+getData();
