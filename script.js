8fbadd91af859d9b4f1d05273a1bd92fec757493
lpiedras4 <piedras.leo@gmail.com>
2026-01-31 19:58:42 -0800

    started displayMeals function

--- script.js
+++ script.js
@@ -10,14 +10,38 @@ const mealDetailsContent = document.querySelector("meal-details-content");
 
 async function searchMeals(keyword) {
     try{
-        resultsGrid.innerHTML('<p>Loading</p>');
+        resultsGrid.innerHTML='<p>Loading</p>';
         const response = await fetch(API_LINK+`s=${keyword}`);
         if (!response.ok) {
             throw new Error("Network response was not ok");
         }
        const data = await response.json();
+       displayMeals(data.meals);
     }catch(error){
         console.error('Error fetching meals: ', error);
         resultsGrid.innerHTML ='<p id="api-message-error"> Sorry, something went wrong. Please try again </p>';
     }
+}
+
+function displayMeals(meals){
+    //loop through meals and create an info card for each one
+    mealsContainer.innerHTML='';
+    meals.forEach((meal =>{
+        mealsContainer.innerHTML+=`
+        <div class="meal" data-meal-id="${meal.id}"> 
+            <img src="${meal.strMealThumb} alt="${meal.strMeal}">
+            <div class="meal-info"> 
+            <h3 class="meal-title">${meal.strMeal}</h3>
+            ${meal.strCategory ? `<div class="meal-category">${meal.strCategory}</div>` : ""}
+            </div>
+        </div>
+        `
+    }));
+    //display meal details
+    mealDetailsContent.innerHTML = `
+    <imga src="${meal.strMealThumb}" alt="${meal.strMeal}" class = "meal-details-img">
+    <h2 class="meal-details-title"></h2>
+    `;
+
+
 }
