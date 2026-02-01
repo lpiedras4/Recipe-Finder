fe724558556c9ad6e33c591f9733b830227bfba0
lpiedras4 <piedras.leo@gmail.com>
2026-01-31 19:32:35 -0800

    added dom elements

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const resultsGrid = document.getElementById("meals");
+const API_LINK = "https://www.themealdb.com/api/json/v1/1/search.php?";
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+const mealsContainer = document.getElementById("meals-container");
+const resultHeading = document.getElementById("result-heading");
+const errorContainer=document.getElementById("error-container");
+const mealDetails = document.getElementById("meal-details");
+const mealDetailsContent = document.querySelector("meal-details-content");
 
 async function searchMeals(keyword) {
     try{
         resultsGrid.innerHTML('<p>Loading</p>');
-        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`);
+        const response = await fetch(API_LINK+`s=${keyword}`);
         if (!response.ok) {
             throw new Error("Network response was not ok");
         }
@@ -13,4 +21,3 @@ async function searchMeals(keyword) {
         resultsGrid.innerHTML ='<p id="api-message-error"> Sorry, something went wrong. Please try again </p>';
     }
 }
-searchMeals("chicken".meals);
